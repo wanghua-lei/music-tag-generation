f8a3161814a06c7a542d0e39dc810234c7a83c98
Hai Carroll <65102355+wanghua-lei@users.noreply.github.com>
2024-05-21 21:23:59 +0800

    Add files via upload

--- classfier.py
+++ classfier.py
@@ -15,42 +15,14 @@ BEATs_model = BEATs(cfg)
 BEATs_model.load_state_dict(checkpoint['model'])
 BEATs_model.eval()
 
-def id_to_name(label_idx):
-    json_path ="ontology/ontology.json"
-    with open(json_path, 'r') as f:
-        data = json.load(f)
-    mapdata = {item["id"]: item["name"] for item in data}
-    result = [mapdata[id] for id in label_idx if id in mapdata]
-    if 'Silence'  in result:
-        result.remove('Silence')
-    if 'Music' in result:
-        result.remove('Music')
-    if 'Speech' in result:
-        result.remove('Speech')
-    if 'Background music' in result:
-        result.remove('Background music') 
-    return result
-# predict the classification probability of each class
+BEATs_model.load_state_dict(checkpoint['model'])
+BEATs_model.eval()
 
-# Function to append data to a JSON file
-def append_to_json_file(json_file, entries):
-    try:
-        # Load existing data from the JSON file
-        with open(json_file, 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        # If file does not exist, start with an empty list
-        data = []
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+BEATs_model.to(device)
 
-    # Append new data
-    data.extend(entries)
-
-    # Write the updated data back to the JSON file
-    with open(json_file, 'w') as file:
-        json.dump(data, file, indent=4)
 
 data_set = AudioDataset()
-
 val_dataloder=DataLoader(data_set,
         batch_size=500,
         num_workers=64,
@@ -69,5 +41,6 @@ with torch.no_grad():
         waveform, wav_path = batch
         probs = BEATs_model.extract_features(waveform)[0]
         all_probs.append(probs)
+
 stacked_probs = torch.vstack(all_probs)
 np.save('probs.npy', stacked_probs.cpu().numpy())
